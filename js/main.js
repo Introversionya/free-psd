f3042f9c5575f1053c6ee5f93c6f95b8b2069d01
Anton Demidenko <yaintroversivnost@gmail.com>
2020-07-29 13:57:02 +0500

    адаптив

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 $(function(){
 
-    $('.slider__inner, .news__slider-inner').slick({
-        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-        infinite: false
-    });
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
 
-    $('select').styler();
+  $('select').styler();
 
-    $('.header__btn-menu').on('click', function(){
-        $('.menu ul').slideToggle();
-    });
-    
-   
-
-    
-
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
 
 });
